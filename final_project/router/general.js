b45033620ca39186228729cf0f57d3dfd11814e3
TheDevJoeT <thedevjoet@gmail.com>
2026-01-02 00:10:10 -0500

    task 11 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,19 +40,24 @@ public_users.get('/', async (req, res) => {
     }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn; // Retrieve ISBN from request parameters
+/// Get book details based on ISBN using async/await + axios
+public_users.get('/isbn/:isbn', async (req, res) => {
+    const isbn = req.params.isbn; // Get ISBN from request parameters
 
-    const book = books[isbn]; // Get the book from books database
+    try {
+        // Simulate fetching the book asynchronously
+        const response = await Promise.resolve({ data: books[isbn] });
 
-    if (book) {
-        res.send(JSON.stringify(book, null, 4)); // Send book details neatly
-    } else {
-        res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
-    };
- 
- });
+        if (response.data) {
+            res.send(JSON.stringify(response.data, null, 4));
+        } else {
+            res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching book by ISBN", error: error.message });
+    }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
