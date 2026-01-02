abfcdbe46d1d3163bcd0be8312bed4d52d27a8ac
TheDevJoeT <thedevjoet@gmail.com>
2026-01-02 00:04:50 -0500

    task 10 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require('axios');
 const public_users = express.Router();
 
 
@@ -27,9 +28,16 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
-  
+public_users.get('/', async (req, res) => {
+    try {
+        // Simulate fetching books via axios (could be an API endpoint)
+        // Here, since books are local, we use a resolved Promise to simulate async
+        const response = await Promise.resolve({ data: books });
+
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching books", error: error.message });
+    }
 });
 
 // Get book details based on ISBN
